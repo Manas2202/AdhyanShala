f6bedc0c3b17c1d11ca81752f043b53c14f9005a
RAJAT BHASKARE <rajatbhaskare@gmail.com>
2021-11-01 15:56:36 -0400

    setup

---
+++ pages/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './../assets/css/style.css';
+// reactstrap components
+import { Card, Container, Row } from "reactstrap";
+// layout for this page
+
+// core components
+import Homes from "layouts/home.js";
+
+function Home() {
+  return (
+    <>
+      {/* Page content */}
+      
+    </>
+  );
+}
+
+
+export default Home;
