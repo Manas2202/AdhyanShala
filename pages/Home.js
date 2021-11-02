d2c926d82a2535a61a05a23202828dafa2c238cc
Mitali0326 <76598739+Mitali0326@users.noreply.github.com>
2021-11-02 14:43:34 +0530

    landing

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './../assets/css/style.css';
-import hero from './../assets/img/landing/Main Landing-01.png';
+import hero from './../assets/img/landing/Main Landing-02.png';
 import p1 from './../assets/img/landing/Main Landing-03.png';
 import p2 from './../assets/img/landing/Main Landing-04.png';
 import p3 from './../assets/img/landing/Main Landing-05.png';
@@ -18,19 +18,24 @@ function Home() {
   return (
     <>
       {/* Page content */}
-      <div className="containers d-flex justify-content-center">
+      <div className="containers d-flex justify-content-center" style={{
+           backgroundImage: `url('${hero}')`,
+          backgroundSize: 'cover',
+          backgroundAttachment: 'fixed',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center'
+      }}>
         <div className="ring">
-          <img  src={hero} alt="not working" />
-          <img src={p1} className="obj1" alt="not working" />
-          <img src={p2} className="obj2" alt="not working" />
-          <img src={p3} className="obj3" alt="not working" />
-          <img src={p4} className="obj1" alt="not working" />
-          <img src={p5} className="obj2" alt="not working" />
-          <img src={p6} className="obj3" alt="not working" />
-          <img src={p7} className="obj1" alt="not working not to move" />
-          <img src={p8} className="obj1" alt="not working" />
-          <img src={p9} className="obj2" alt="not working" />
-          <img src={p10}  className="obj3" alt="not working" />
+            {/* <img src={p1} className="obj1" alt="not working" />
+            <img src={p2} className="obj2" alt="not working" />
+            <img src={p3} className="obj3" alt="not working" />
+            <img src={p4} className="obj1" alt="not working" />
+            <img src={p5} className="obj2" alt="not working" />
+            <img src={p6} className="obj3" alt="not working" />
+            <img src={p7} className="obj1" alt="not working not to move" />
+            <img src={p8} className="obj1" alt="not working" />
+            <img src={p9} className="obj2" alt="not working" />
+            <img src={p10}  className="obj3" alt="not working" /> */}
         </div>
       </div>
       
