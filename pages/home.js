79962a8906cd5d653a38257e51ccdaa8ea623084
Mitali0326 <76598739+Mitali0326@users.noreply.github.com>
2021-11-02 13:24:24 +0530

    HOME RATIO

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './../assets/css/style.css';
-import hero from './../assets/img/landing/Main Landing-02.png';
+import hero from './../assets/img/landing/Main Landing-01.png';
 import p1 from './../assets/img/landing/Main Landing-03.png';
 import p2 from './../assets/img/landing/Main Landing-04.png';
 import p3 from './../assets/img/landing/Main Landing-05.png';
@@ -12,18 +12,13 @@ import p8 from './../assets/img/landing/Main Landing-10.png';
 import p9 from './../assets/img/landing/Main Landing-11.png';
 import p10 from './../assets/img/landing/Main Landing-12.png';
 
-// reactstrap components
-import { Card, Container, Row } from "reactstrap";
-// layout for this page
 
-// core components
-import Homes from "layouts/home.js";
 
 function Home() {
   return (
     <>
       {/* Page content */}
-      <div className="containers d-flex align-items-center">
+      <div className="containers d-flex justify-content-center">
         <div className="ring">
           <img  src={hero} alt="not working" />
           <img src={p1} className="obj1" alt="not working" />
@@ -36,7 +31,6 @@ function Home() {
           <img src={p8} className="obj1" alt="not working" />
           <img src={p9} className="obj2" alt="not working" />
           <img src={p10}  className="obj3" alt="not working" />
-  
         </div>
       </div>
       
