93f9e171abdbd90c418f6e99bf25f5c10d75a6f8
Mitali0326 <76598739+Mitali0326@users.noreply.github.com>
2021-11-08 14:39:23 +0530

    images removed

--- pages/start.js
+++ pages/start.js
@@ -3,9 +3,6 @@ import './../assets/css/style.css';
 import Home from './home.js';
 import AwesomeSlider from 'react-awesome-slider';
 import hero from './../assets/img/landing/Main Landing-13.png';
-import p8 from './../assets/img/landing/Main Landing-10.png';
-import p9 from './../assets/img/landing/Main Landing-11.png';
-import p10 from './../assets/img/landing/Main Landing-12.png';
 import 'react-awesome-slider/dist/styles.css';
 function Start() {
   return (
